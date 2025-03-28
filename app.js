7683bba0bc6087e8023db6c482fffd394c3e81ed
Chinguun19 <centillionc0919@gmail.com>
2025-03-28 13:33:12 +0800

    final

--- app.js
+++ app.js
@@ -1,281 +1,128 @@
-let inputBox = document.getElementById("input-box")
-let editBox = document.getElementById("edit-box")
-let listContainer = document.getElementById("list-container");
-let editOpenBtn = document.getElementById("edid-modal")
-//let editCloseBtn = document.getElementById("edit-modal")
-let openBtn = document.getElementById("openModal");
-let closeBtn = document.getElementById("closeModal");
-let closeEditModal = document.getElementById("closeModalEdit")
-let editModal = document.getElementById("edit-modal")
-let modal = document.getElementById("modal");
-let option = document.getElementById("option")
-let editOption = document.getElementById("editOption")
-let doneCard = document.querySelector('#doneCard').querySelector('.list-container')
-let blockedCard = document.querySelector('#blockedCard').querySelector('.list-container')
-let todoCard = document.getElementById("toDoCard")
-let progressCard = document.querySelector('#progressCard').querySelector('.list-container')
-let taskCount = listContainer.children.length;
+const $ = id => document.getElementById(id);
+const $q = selector => document.querySelector(selector);
 
+const elements = {
+    inputBox: $('input-box'),
+    editBox: $('edit-box'),
+    listContainer: $('list-container'),
+    modal: $('modal'),
+    editModal: $('edit-modal'),
+    openBtn: $('openModal'),
+    closeBtn: $('closeModal'),
+    closeEditModal: $('closeModalEdit'),
+    option: $('option'),
+    editOption: $('editOption'),
+    cards: {
+        todo: $('list-container'),
+        progress: $q('#progressCard .list-container'),
+        done: $q('#doneCard .list-container'),
+        blocked: $q('#blockedCard .list-container')
+    }
+};
 
 function updateCounter() {
-    const taskCount = listContainer.children.length; 
-    document.getElementById('count').textContent = taskCount;
+    $('count').textContent = elements.listContainer.children.length;
 }
 
+function createTaskElement(text) {
+    const li = document.createElement('li');
+    li.innerHTML = text;
+    
+    const span = document.createElement('span');
+    const editButton = document.createElement('div');
+    editButton.classList.add('editButton');
+    
+    li.appendChild(span);
+    li.appendChild(editButton);
+    
+    return li;
+}
 
-
-
-openBtn.addEventListener("click", () => {
-    modal.classList.add("open"); 
-})
-
-closeBtn.addEventListener("click", () => {
-    modal.classList.remove("open");
-})
-closeEditModal.addEventListener("click", () =>{
-    editModal.classList.remove("open");
-})
-
-
-
-function addTask () {
-    if(inputBox.value === ''){
-        alert(`Write something`);
-    }
-    else if(option.value == "Todo"){
-
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "";
-        li.appendChild(span);
-        let newDiv = document.createElement('div');
-        newDiv.classList.add('editButton');
-        li.appendChild(newDiv);
-        taskCount++;
-        updateCounter()
-        saveData();
-
-    }
-    else if(option.value == "inprogress"){
-
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        progressCard.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "";
-        li.appendChild(span);
-        let newDiv = document.createElement('div');
-        newDiv.classList.add('editButton');
-        li.appendChild(newDiv);
-
-        saveData();
-
-    }
-    else if(option.value == "done"){
-
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        doneCard.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "";
-        li.appendChild(span);
-        let newDiv = document.createElement('div');
-        newDiv.classList.add('editButton');
-        li.appendChild(newDiv);
-
-        saveData();
-
-    }
-    else if(option.value == "blocked"){
-
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        blockedCard.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "";
-        li.appendChild(span);
-        let newDiv = document.createElement('div');
-        newDiv.classList.add('editButton');
-        li.appendChild(newDiv);
-
-        saveData();
-
+function addTask() {
+    if (!elements.inputBox.value) {
+        alert('Write something');
+        return;
     }
 
+    const text = elements.inputBox.value;
+    const targetCard = {
+        'Todo': elements.cards.todo,
+        'inprogress': elements.cards.progress,
+        'done': elements.cards.done,
+        'blocked': elements.cards.blocked
+    }[elements.option.value];
 
+    if (targetCard) {
+        const taskElement = createTaskElement(text);
+        targetCard.appendChild(taskElement);
+    }
 
-    inputBox.value = "";
+    elements.inputBox.value = '';
     saveData();
 }
 
-doneCard.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData()
-    }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        saveData();
-    } else if (e.target.classList.contains("editButton")) {
-        editModal.classList.add("open");
-        const li = e.target.parentElement;
-        editBox.value = li.innerText
-        closeEditModal.onclick = function() {
-            li.innerText = editBox.value;
-            let span = document.createElement("span");
-            span.innerHTML = "";
+function handleCardClick(e) {
+    const target = e.target;
+    
+    if (target.tagName === 'LI') {
+        target.classList.toggle('checked');
+    } else if (target.tagName === 'SPAN') {
+        target.parentElement.remove();
+    } else if (target.classList.contains('editButton')) {
+        elements.editModal.classList.add('open');
+        const li = target.parentElement;
+        elements.editBox.value = li.innerText;
+
+        elements.closeEditModal.onclick = () => {
+            li.innerText = elements.editBox.value;
+            
+            const span = document.createElement('span');
+            const editButton = document.createElement('div');
+            editButton.classList.add('editButton');
+            
             li.appendChild(span);
-            let newDiv = document.createElement('div');
-            newDiv.classList.add('editButton');
-            li.appendChild(newDiv);
-            if(editOption.value == "inprogress"){
-                progressCard.appendChild(li)
-            } else if (editOption.value == "done") {
-                doneCard.appendChild(li)
-            } else if (editOption.value == "blocked"){
-                blockedCard.appendChild(li)
-            } else if (editOption.value == "Todo"){
-                listContainer.appendChild(li)
-            }
-        };
-        
-    }  
-    saveData()
-}, false);
+            li.appendChild(editButton);
 
-progressCard.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData()
+            const targetCard = {
+                'inprogress': elements.cards.progress,
+                'done': elements.cards.done,
+                'blocked': elements.cards.blocked,
+                'Todo': elements.cards.todo
+            }[elements.editOption.value];
+
+            targetCard.appendChild(li);
+            elements.editModal.classList.remove('open');
+        };
     }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        saveData();
-    } else if (e.target.classList.contains("editButton")) {
-        editModal.classList.add("open");
-        const li = e.target.parentElement;
-        editBox.value = li.innerText
-        closeEditModal.onclick = function() {
-            li.innerText = editBox.value;
-            let span = document.createElement("span");
-            span.innerHTML = "";
-            li.appendChild(span);
-            let newDiv = document.createElement('div');
-            newDiv.classList.add('editButton');
-            li.appendChild(newDiv);
-            if(editOption.value == "inprogress"){
-                progressCard.appendChild(li)
-            } else if (editOption.value == "done") {
-                doneCard.appendChild(li)
-            } else if (editOption.value == "blocked"){
-                blockedCard.appendChild(li)
-            } else if (editOption.value == "Todo"){
-                listContainer.appendChild(li)
-            }
-        };
-        
-    }  
-    saveData()
-}, false);
 
-blockedCard.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData()
-    }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        saveData();
-    } else if (e.target.classList.contains("editButton")) {
-        editModal.classList.add("open");
-        const li = e.target.parentElement;
-        editBox.value = li.innerText
-        closeEditModal.onclick = function() {
-            li.innerText = editBox.value;
-            let span = document.createElement("span");
-            span.innerHTML = "";
-            li.appendChild(span);
-            let newDiv = document.createElement('div');
-            newDiv.classList.add('editButton');
-            li.appendChild(newDiv);
-            if(editOption.value == "inprogress"){
-                progressCard.appendChild(li)
-            } else if (editOption.value == "done") {
-                doneCard.appendChild(li)
-            } else if (editOption.value == "blocked"){
-                blockedCard.appendChild(li)
-            } else if (editOption.value == "Todo"){
-                listContainer.appendChild(li)
-            }
-        };
-        
-    }  
-    saveData()
-}, false);
-
-
-
-
-
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData()
-    }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        taskCount --;
-        updateCounter()
-        saveData();
-    } else if (e.target.classList.contains("editButton")) {
-        editModal.classList.add("open");
-        const li = e.target.parentElement;
-        editBox.value = li.innerText
-        closeEditModal.onclick = function() {
-            li.innerText = editBox.value;
-            let span = document.createElement("span");
-            span.innerHTML = "";
-            li.appendChild(span);
-            let newDiv = document.createElement('div');
-            newDiv.classList.add('editButton');
-            li.appendChild(newDiv);
-            if(editOption.value == "inprogress"){
-                progressCard.appendChild(li)
-            } else if (editOption.value == "done") {
-                doneCard.appendChild(li)
-            } else if (editOption.value == "blocked"){
-                blockedCard.appendChild(li)
-            } else if (editOption.value == "Todo"){
-                listContainer.appendChild(li)
-            }
-        };
-        
-    }  
-
-    saveData()  
-
-}, false);
-
-
-
-
-
-function saveData(){
-    updateCounter()
-    localStorage.setItem("data1", listContainer.innerHTML);
-    localStorage.setItem("data2", doneCard.innerHTML);
-    localStorage.setItem("data3",  progressCard.innerHTML);
-    localStorage.setItem("data4",  blockedCard.innerHTML);
-  
+    saveData();
 }
-function showTask(){
-    updateCounter()
-    listContainer.innerHTML = localStorage.getItem("data1");
-    doneCard.innerHTML = localStorage.getItem("data2");
-    progressCard.innerHTML = localStorage.getItem("data3");
-    blockedCard.innerHTML = localStorage.getItem("data4");
-}
-showTask()
 
+function saveData() {
+    updateCounter();
+    Object.keys(elements.cards).forEach((key, index) => {
+        localStorage.setItem(`data${index + 1}`, elements.cards[key].innerHTML);
+    });
+}
+
+function showTasks() {
+    Object.keys(elements.cards).forEach((key, index) => {
+        elements.cards[key].innerHTML = localStorage.getItem(`data${index + 1}`) || '';
+    });
+    updateCounter();
+}
+
+// Event Listeners
+elements.openBtn.addEventListener('click', () => elements.modal.classList.add('open'));
+elements.closeBtn.addEventListener('click', () => elements.modal.classList.remove('open'));
+elements.closeEditModal.addEventListener('click', () => elements.editModal.classList.remove('open'));
+
+Object.values(elements.cards).forEach(card => {
+    card.addEventListener('click', handleCardClick);
+});
+
+
+$('buttonAdd').addEventListener('click', addTask);
+
+// Initialize
+showTasks();
